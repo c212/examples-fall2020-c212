6648468ee030fa4fc17288c53c5774dd9a8fb15b
c212 <40395648+c212@users.noreply.github.com>
2020-09-14 16:12:01 -0400

    getting input
    from the command line

---
+++ lecture07/Something.java
@@ -0,0 +1,9 @@
+public class Something {
+  public static void main(String[] args) {
+    String time1, time2; 
+    time1 = args[0];
+    time2 = args[1]; 
+    System.out.println("The first time is: " + time1);   
+    System.out.println("The second time: " + time1);   
+  }
+}
