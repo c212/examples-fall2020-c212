b8fcf9f70e1016a85547eae97bacac70b3c73088
c212 <40395648+c212@users.noreply.github.com>
2020-09-21 10:22:53 -0400

    creating custom cars
    cars have fuel and efficiency inside

--- lecture09/Car.java
+++ lecture09/Car.java
@@ -1,3 +1,11 @@
 public class Car {
+    private int efficiency; // miles/gallon
+    private double fuel; // gallons
+    // both are initialized to 0 (zero) as instance variables
+    public Car(int efficiency) {
+      this.efficiency = efficiency; // use this please for instance variables
+      // of course if the two names were different I would not have to use this
+      // but I recommend even in that case you do that to emphasize the type of variable
+    }
     
 }
