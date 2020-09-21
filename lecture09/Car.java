36e438446812800b2bc6722931266fca43df27a1
c212 <40395648+c212@users.noreply.github.com>
2020-09-21 10:35:18 -0400

    unit testing (driving)
    this is a car that can drive on borrowed fuel

--- lecture09/Car.java
+++ lecture09/Car.java
@@ -7,5 +7,16 @@ public class Car {
       // of course if the two names were different I would not have to use this
       // but I recommend even in that case you do that to emphasize the type of variable
     }
-    
+    public void drive(double miles) { // how far you want to go in miles
+      this.fuel = this.fuel - miles / this.efficiency; // think about this formula
+    }
+    public static void main(String[] args) {
+      Car a = new Car(50); // create a new Car
+      System.out.println( a ); // expect Car@abf0356e or something to that effect 
+      System.out.println( a.fuel ); // expect: 0.0
+      System.out.println( a.efficiency ); // expect: 50
+      a.drive(100); // drive 100 miles
+      System.out.println( a.fuel ); // expect: -2.0
+      System.out.println( a.efficiency ); // expect: 50
+    } // this is for this stage essentially Testing R3.18 (but for this stage!) 
 }
